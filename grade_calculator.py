1b3c3f137219d9a7cdeff7ffa47ed6d79953ad9c
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-08-15 13:20:32 +0600

    grade_calculator (Solved)

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,12 +1,17 @@
 marks = int(input("Enter Your Marks : "))
 
-if marks >= 80:
-    print("A+")
+if marks > 100 or marks < 0:
+    print("Wrong Input! Please, Enter Your Marks. Which Is 1 to 100")
+elif marks >= 80:
+    print("Your Grade Is : A+")
 elif marks >= 70:
-    print("A")
+    print("Your Grade Is : A")
 elif marks >= 60:
-    print("A-")
+    print("Your Grade Is : A-")
 elif marks >= 50:
-    print("B")
+    print("Your Grade Is : B")
 elif marks >= 40:
-    print("C")
+    print("Your Grade Is : C")
+elif marks >= 33:
+    print("Your Grade Is : D")
+else: print("Your Grade Is : F")
