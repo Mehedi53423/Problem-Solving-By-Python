5d29ecb4db5f2ffbc8a0ef2961abe64975f75057
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-08-15 13:09:40 +0600

    Initial Commit

---
+++ grade_calculator.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter Your Marks : "))
+
+if marks >= 80:
+    print("A+")
+elif marks >= 70:
+    print("A")
+elif marks >= 60:
+    print("A-")
+elif marks >= 50:
+    print("B")
+elif marks >= 40:
+    print("C")
